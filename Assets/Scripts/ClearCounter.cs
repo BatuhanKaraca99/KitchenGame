7b7a61cbacd401fdfb3bba97840952d6e8ba4010
Batuhan KARACA <b.karaca.1999@gmail.com>
2023-12-30 18:18:23 +0300

    Player Pick up, Drop Objects

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -8,7 +8,30 @@ public class ClearCounter : BaseCounter
 
     public override void Interact(Player player)
     {
-        Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab);
-        kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(player);
+        if (!HasKitchenObject())
+        {
+            // There is no KitchenObject here
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying something
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            } else
+            {
+                // Player not carrying anything
+
+            }
+        } 
+        else 
+        {
+            // There is a KitchenObject here
+            if(player.HasKitchenObject())
+            {
+                // Player is carrying something
+            } else
+            {
+                // Player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
